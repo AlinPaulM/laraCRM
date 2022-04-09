e34ad4b3718b589920bf3d6d8e5921769e7ce8ec
Alin M <macoveialinp@gmail.com>
2022-04-09 18:55:46 +0100

    updated employees and companies migrations

--- database/migrations/2022_04_05_225730_create_companies_table.php
+++ database/migrations/2022_04_05_225730_create_companies_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id(); // auto-incrementing unsigned BIGINT primary key https://laravel.com/docs/9.x/migrations#column-method-id
             $table->string('name', 100); // most columns are required(i.e. NOT NULL) by default in laravel
-            $table->string('email')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->string('logo')->nullable();
-            $table->string('website')->nullable();
+            $table->string('website')->unique()->nullable();
             $table->timestamps();
 
             $table->engine = 'InnoDB'; // InnoDB is the default storage engine since MySQL 5.5(2010), and this project was created with MySQL 5.7.31, so this is not needed, i just added it for learning purposes
--- database/migrations/2022_04_05_225938_create_employees_table.php
+++ database/migrations/2022_04_05_225938_create_employees_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('first_name', 100);
             $table->string('last_name', 100);
-            $table->string('email')->nullable();
-            $table->string('phone', 15)->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('phone', 15)->unique()->nullable();
             $table->timestamps();
 
             $table->foreignId('company_id')->constrained('companies');
